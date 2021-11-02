5ba39733e061469b6ec822f6aac0f7139d1fb437
Colditz Colligula <79027579+ColditzColligula@users.noreply.github.com>
2021-11-02 19:47:23 +0100

    Add files via upload

--- hashcalc.py
+++ hashcalc.py
@@ -1,43 +1,65 @@
-#importing main functionality
+# HashCalc version 0.2
+
+#   importing main functionality
+import PySimpleGUI as sg
 import hashlib
 import os.path
 
-#calculating MD5
+
+#   calculating MD5
 def calculate_checksummd5(filename):
     with open(filename, 'rb') as file:
         return hashlib.md5(file.read()).hexdigest()
         
-#calculating SHA-1
+#   calculating SHA-1
 def calculate_checksum1(filename):
     with open(filename, 'rb') as file:
         return hashlib.sha1(file.read()).hexdigest()
 
-#calculating SHA-256
+#   calculating SHA-256
 def calculate_checksum256(filename):
     with open(filename, 'rb') as file:
         return hashlib.sha256(file.read()).hexdigest()
 
-#description
-print('\nThis Python Script will calculate Checksums (MD5, SHA-1, SHA-256) for a given file.\n'
-'Input path to file below\n')
 
-#User will input path here
-path = str(input('>>>'))
+#   defining window colour
+sg.theme('BrownBlue')
+#   Content of the GUI
+layout = [  [sg.Text('HashCalc')],
+            [sg.Text('Enter full file-path'), sg.InputText()],
+            [sg.Button('Calculate'), sg.Button('Close')] ]
 
-#checking if path is correct and leads to a file
-try:
-    calculate_checksum1(path)
-#error-handling if path to file incorrect or not found
-except (FileNotFoundError, SyntaxError, NameError):
-    print('File not found! Please check the path!')
-#printing output
-else:
-    print('\nMD5 Checksum:')
-    md5 = (calculate_checksummd5(path))
-    print(md5)
-    print('\nSHA-1 Checksum:')
-    sha1 = (calculate_checksum1(path))
-    print(sha1)
-    print('\nSHA-256 Checksum:')
-    sha256 = (calculate_checksum256(path))
-    print(sha256)
+#   Creating GUI Window
+window = sg.Window('HashCalc v0.2', layout)
+
+#   Loop to process events and get input value
+while True:
+    event, values = window.read()
+    if event == sg.WIN_CLOSED or event == 'Close': 
+        #   if user closes window or clicks cancel
+        break
+#   Calculation
+    if event == 'Calculate':
+        
+        #   splitting Filepath down to just the filename
+        target = values[0]
+        tcut = target.split("/", -1)[-1]
+        print('\nCalculating hashsums for ', tcut)
+        path = str(values[0])
+        try:
+            calculate_checksum1(path)
+        #   error-handling if path to file incorrect or not found
+        except (FileNotFoundError, SyntaxError, NameError):
+            print('File not found! Please check the path!')
+        #   printing output
+        else:
+            print('\nMD5 Checksum:')
+            md5 = (calculate_checksummd5(path))
+            print(md5)
+            print('\nSHA-1 Checksum:')
+            sha1 = (calculate_checksum1(path))
+            print(sha1)
+            print('\nSHA-256 Checksum:')
+            sha256 = (calculate_checksum256(path))
+            print(sha256)
+window.close()
